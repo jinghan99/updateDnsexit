6b13f7e098c6cd7c8e94d2d30f22a3c1ed4e3546
805@lhtjf.com <Jinghan940115>
2019-08-16 10:59:53 +0800

    添加时区

--- src/main/java/com/girl/update/UpdateApplication.java
+++ src/main/java/com/girl/update/UpdateApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
 import java.util.TimeZone;
 
 /**
@@ -19,6 +20,12 @@ public class UpdateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UpdateApplication.class, args);
 	}
-
+	@PostConstruct
+	void started() {
+//		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+//		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+		System.out.println(new Date());
+	}
 }
 
